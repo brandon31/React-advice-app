f15a2d9a6af99a11c6b8e9462e8e9b2dbea8eae1
Brandon <106634008+brandon31@users.noreply.github.com>
2022-10-27 01:20:01 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state =  { advice: '', id: '' };
+
+  fetchAdvice = () => {
+    axios.get("	https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice } = response.data.slip;
+        const { id } = response.data.slip;
+        
+        this.setState({ advice });
+        this.setState({ id });
+
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  render() {
+
+    return (
+      <>
+      <div className='wrapper'>
+      <h1>Random Advice generator</h1>
+      <div className='container'>
+         <p>ADVICE #{this.state.id}</p>
+         <p>"{this.state.advice}"</p>
+         <button className='material-symbols-outlined' onClick={this.fetchAdvice}>
+            casino
+          </button>
+      <p>Press the dice for a new Advice</p>
+      </div>
+      </div>
+      </>
+    );
+
+  }
 }
 
 export default App;
